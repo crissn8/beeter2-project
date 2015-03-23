b97485446385fb042ae5144d47b4c840fbf50393
cristina sanchez <cristina.s.narbon@gmail.com>
2015-03-23 23:27:50 +0100

    lkdja

---
+++ beeter2-api/src/main/java/edu/upc/eetac/dsa/csanchez/beeter2/api/BeeterRootAPIResource.java
@@ -0,0 +1,17 @@
+package edu.upc.eetac.dsa.csanchez.beeter2.api;
+
+	import javax.ws.rs.GET;
+	import javax.ws.rs.Path;
+	 
+	import edu.upc.eetac.dsa.csanchez.beeter2.api.model.BeeterRootAPI;
+	 
+	@Path("/")
+	public class BeeterRootAPIResource {
+		@GET
+		public BeeterRootAPI getRootAPI() {
+			BeeterRootAPI api = new BeeterRootAPI();
+			return api;
+		}
+	}
+
+
---
+++ beeter2-api/src/main/java/edu/upc/eetac/dsa/csanchez/beeter2/api/MediaType.java
@@ -0,0 +1,9 @@
+package edu.upc.eetac.dsa.csanchez.beeter2.api;
+
+public interface MediaType {
+	public final static String BEETER_API_USER = "application/vnd.beeter2.api.user+json";
+	public final static String BEETER_API_USER_COLLECTION = "application/vnd.beeter2.api.user.collection+json";
+	public final static String BEETER_API_STING = "application/vnd.beeter2.api.sting+json";
+	public final static String BEETER_API_STING_COLLECTION = "application/vnd.beeter2.api.sting.collection+json";
+	public final static String BEETER_API_ERROR = "application/vnd.dsa.beeter2.error+json";
+}
